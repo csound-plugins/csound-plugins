d36f96fc6191ce6301b129894a1980db5b42060c
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-04-24 19:55:15 +0200

    trying also to fix windows

--- azure/buildWin64.ps1
+++ azure/buildWin64.ps1
@@ -1,9 +1,9 @@
 $env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
 
-dir  
+dir
 cd c:/
 Invoke-WebRequest -Uri "https://github.com/csound/csound/releases/download/6.14.0/Csound6.14.0-Windows_x64-binaries.zip"
-7z.exe x Csound6.14.0-Windows_x64-binaries -o"C:/Program Files/Csound6_x64"
+7z.exe x Csound6.14.0-Windows_x64-binaries.zip -o"C:/Program Files/Csound6_x64"
 cd "C:/Program Files/Csound6_x64"
 dir
 
@@ -15,4 +15,3 @@ cmake -G "Visual Studio 15 2017 Win64" -DBUILD_JSUSFX_OPCODES=OFF ..
 msbuild.exe Project.sln /property:Platform=x64 /property:Configuration=Release
 mkdir D:/a/1/a/Win64
 Copy-Item "D:/a/1/s/build/Release/*" -Destination D:/a/1/a/Win64
-
