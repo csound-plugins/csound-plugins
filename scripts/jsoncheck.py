494b0fcffc579e9c4fcc4abac2fcee3b608fff0a
gesellkammer <eduardo.moguillansky@gmail.com>
2023-04-10 14:21:59 +0200

    check ok

--- scripts/jsoncheck.py
+++ scripts/jsoncheck.py
@@ -6,11 +6,13 @@ def print_with_linenumbers(s):
     for i, line in enumerate(s.splitlines()):
         print(f"{i+1:003d} {line}")
 
+
 jsonfile = sys.argv[1]
 s = open(jsonfile).read()
 try:
     d = json.loads(s)
     print("ok")
+    sys.exit(0)
 except Exception as e:
     print_with_linenumbers(s)
     print(e)
